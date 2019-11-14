5c82fcc5fc8bdc45d1e004059ff78ed318110469
Alexandre94H <alexandre@bithub.codes>
2019-11-14 17:16:49 +0100

    buff <= 0 edited

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/09 19:32:06 by ahallain          #+#    #+#             */
-/*   Updated: 2019/11/13 18:57:18 by ahallain         ###   ########.fr       */
+/*   Updated: 2019/11/13 19:29:07 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,37 @@
 #include <unistd.h>
 #include <string.h>
 
-int		main(int argc, char **argv)
-{
+void	readfile(char *file, int print) {
 	int		fd;
 	int		ret;
 	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return ;
+	ret = 1;
+	while (ret > 0)
+	{
+		ret = get_next_line(fd, &line);
+		if (print)
+			printf("%s\n", line);
+		free(line);
+	}
+	close(fd);
+}
+
+int		main(int argc, char **argv)
+{
+	int		index;
 	int		ex;
 	char	*leaks;
 
 	if (argc < 3)
 		return (0);
-	ex = atoi(argv[2]);
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	ret = 1;
-	while (ret > 0)
-	{
-		ret = get_next_line(fd, &line);
-		if (!ex)
-			printf("%s\n", line);
-		free(line);
-	}
-	close(fd);
+	index = 0;
+	ex = atoi(argv[argc - 1]);
+	while(++index < argc)
+		readfile(argv[index], !ex);
 	if (ex) {
 		if (!(leaks = malloc(7)))
 			return (2);
@@ -49,13 +57,13 @@ int		main(int argc, char **argv)
 		leaks[3] = 'k';
 		leaks[4] = 's';
 		leaks[5] = ' ';
-		ex = 2;
-		while (argv[0][ex])
+		index = 2;
+		while (argv[0][index])
 		{
-			leaks[ex + 4] = argv[0][ex];
-			ex++;
+			leaks[index + 4] = argv[0][index];
+			index++;
 		}
-		leaks[ex + 4] = 0;
+		leaks[index + 4] = 0;
 		system(leaks);
 	}
 	return (1);
